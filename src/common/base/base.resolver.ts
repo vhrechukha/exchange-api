fb1cb2755c338fa23d615cb6c7a5d1064324b3ea
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-08-27 14:00:44 +0200

    Added BaseResolver and BaseService

---
+++ src/common/base/base.resolver.ts
@@ -0,0 +1,43 @@
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { type BaseEntity } from 'typeorm';
+
+import { ClassType } from '../types/class-type';
+
+export function BaseResolver<T extends ClassType>(
+  EntityClass: T,
+  EntityDTOClass: any,
+  EntityUpdateDTOClass: any,
+  EntityService: any,
+) {
+  @Resolver({ isAbstract: true })
+  abstract class BaseResolverHost {
+    protected constructor(readonly _entityService: typeof EntityService) {}
+
+    @Query(() => [EntityClass], { name: `${EntityClass.name.toLowerCase()}s` })
+    async findAll(): Promise<BaseEntity[]> {
+      return await this._entityService.findAll();
+    }
+
+    @Query(() => EntityClass, { name: `${EntityClass.name.toLowerCase()}` })
+    async findOne(@Args('id', { type: () => Int }) id: number): Promise<BaseEntity> {
+      return await this._entityService.findOne(id);
+    }
+
+    @Mutation(() => EntityClass)
+    async create(@Args('createInput') createInput: typeof EntityDTOClass): Promise<BaseEntity> {
+      return await this._entityService.create(createInput);
+    }
+
+    @Mutation(() => EntityClass)
+    async update(@Args('updateInput') updateInput: typeof EntityUpdateDTOClass): Promise<BaseEntity> {
+      return await this._entityService.update(updateInput);
+    }
+
+    @Mutation(() => Boolean)
+    async remove(@Args('id', { type: () => Int }) id: number): Promise<boolean> {
+      return await this._entityService.remove(id);
+    }
+  }
+
+  return BaseResolverHost;
+}
---
+++ src/common/base/base.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DeepPartial, Repository } from 'typeorm';
+
+@Injectable()
+export abstract class BaseService<T = any> {
+  protected constructor(readonly _entity: { new (): T }, private readonly _repository: Repository<T>) {}
+
+  async create(createInput: DeepPartial<T>): Promise<T> {
+    const entity = this._repository.create(createInput);
+    return await this._repository.save(entity);
+  }
+
+  async findAll(): Promise<T[]> {
+    return await this._repository.find();
+  }
+
+  async findOne(id: number): Promise<T> {
+    const entity = await this._repository.findOneBy({ id } as any);
+    if (!entity) {
+      throw new NotFoundException(`${this._entity.name} not found`);
+    }
+    return entity;
+  }
+
+  async update(id: number, updateInput: DeepPartial<T>): Promise<T> {
+    await this.findOne(id); // to check if entity exists
+    await this._repository.update(id, updateInput as any); // Casting to any
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<boolean> {
+    const result = await this._repository.delete(id);
+    return result.affected > 0;
+  }
+}
